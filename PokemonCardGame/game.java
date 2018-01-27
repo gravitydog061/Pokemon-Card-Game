686da8ad95b60c20e3074454e73bdb2d417d762f
Zach Zimmerman <zzimmerman-student@nfcs.k12.pa.us>
2018-01-27 14:48:39 -0500

    Add files via upload
    Keep 2 players as it is easier to work with. No defend just attack and item

--- PokemonCardGame/game.java
+++ PokemonCardGame/game.java
@@ -9,56 +9,48 @@ public class game
 {
     static deck d = new deck();
     //Determines number of players
-    static int numOfPlayers = 3;
+    static int numOfPlayers = 2;
     //Simple "coin toss" to decide who goes first
     static int playerTurn = (int)(Math.random() * numOfPlayers);
-
+    static int currentPlayer;
     public static void play(){
         d.shuffle(d.pokemon);
         player p1 = new player();
         player p2 = new player();
-        player p3 = new player();
         //give players pokemon
         System.out.println("---Pokemon Names---");//seperation code
         p1.setName(d.pokemon[0]);
         p2.setName(d.pokemon[1]);
-        p3.setName(d.pokemon[2]);
         //Game is active until one player's health = 0
-        while (p1.health >= 0 || p2.health >= 0 || p3.health >= 0){
+        while (p1.health >= 0 || p2.health >= 0){
             //tells program who's turn it is
             if (playerTurn == 0){ 
                 p1.turn = true;
+                currentPlayer = 0;
             }
             else if (playerTurn == 1){
                 p2.turn = true;
-            }
-            else{
-                p3.turn = true;
+                currentPlayer = 1;
             }
 
             while (p1.turn){
-                System.out.print("Player 1, would you like to attack, defend, or use item? ");
+                System.out.print("Player 1, would you like to attack or use item?");
                 p1.turn();
-                playerTurn = 1;
+                p1.turn = false;
                 p2.turn = true;
+                currentPlayer = 1;
                 break;
             }
 
             while (p2.turn){
-                System.out.print("Player 2, would you like to attack, defend, or use item? ");
+                System.out.print("Player 2, would you like to attack or use item?");
                 p2.turn();
                 p2.turn = false;
-                p3.turn = true;
+                p1.turn = true;
+                currentPlayer = 0;
                 break;
             }
 
-            while (p3.turn){
-                System.out.print("Player 3, would you like to attack, defend, or use item? ");
-                p3.turn();
-                p3.turn = false;
-                p1.turn = true;
-                break;
-            }
         }
     }
 }
--- PokemonCardGame/player.java
+++ PokemonCardGame/player.java
@@ -5,122 +5,66 @@
  * @author Zach Zimmerman
  * @version 1.24.18
  */
-import java.util.Arrays;
-public class player
+public class player extends game
 {
-    //references objects without wiping current data?
     player p1;
     player p2;
-    player p3;
     //base 100 health
     int health = 100;
-    //difference between max health and player health if current healthis greater than 80.
-    int margin;
-    //referencing objects
     deck d = new deck();
     scanner user = new scanner();
-    //variables only known to player
     private String name;
-    private String pkmnName;
+    //hardcoded as a temp user choice
     private String choice;
-    private boolean shadow = false;
-    int shadowCount = 1;
     //says when it's a player's turn
     boolean turn = false;
+    private int margin = 0;
     //gives player's pokemon names
     public void setName(String t){
         name = t;
-        System.out.println("Pokemon name: " + name);
+        System.out.println(name + " " + health + " health "); //test code?
     }
-    //attacks player
+
     public void attack(){
         //random damage
         int damage = (int) (Math.random() * 15 + 15); //+ 15 is needed to keep damage from being 0. range = 15 to 30.
-        //if player is not "Shadowed" then attack else print "Player is defended"
-        if (Arrays.asList(d.pokemon).indexOf(pkmnName) == 0){
-            if (!p1.shadow){
-                p1.health = p1.health - damage;
-                System.out.println(name + " dealt " + damage + " damage to " + p1.name + ".\n" + p1.name +
-                    "'s health is " + p1.health);
-            }
-            else{
-                System.out.print("Player 1 has defended. " + name + "'s attack had no effect.");
-            }
-        }
-        else if (Arrays.asList(d.pokemon).indexOf(pkmnName) == 1){
-            if (!p2.shadow){
-                p2.health = p2.health - damage;
-                System.out.println(name + " dealt " + damage + " damage to " + p2.name +
-                    ".\n" + p2.name + "'s health is " + p2.health);
-            }
-            else{
-                System.out.print("Player 2 has defended. " + name + "'s attack had no effect.");
-            }
-        }   
-        else{
-            if (!p3.shadow){
-                p3.health = p3.health - damage;
-                System.out.println(name + " dealt " + damage + " damage to " + p3.name + ".\n" 
-                    + p3.name + "'s health is " + p3.health);
-            }
-            else{
-                System.out.print("Player 3 has defended. " + name + "'s attack had no effect.");
-            }
-        }
-        //attack a specific pokemon
-        }
-    //protects player for the next player's turn
-    public void defend(){
-        //player becomes "invinsible" for the next player's turn. Ex) P1 defends, P2 cannot attack P1 but P3 can
-        //Cannot be use twice in a row. If used twice print p.name + " defended but it failed." and end turn
-        if (shadowCount != 2){
-            shadow = true;
-            shadowCount++;
+        if (currentPlayer == 0){
+            p2.health = p2.health - damage;
+            System.out.println(p2.name + "'s health is at " + p2.health + ".");
         }
         else{
-            System.out.println(name + " defended but it failed.");
-            shadowCount = 1;
+            p1.health = p1.health - damage;
+            System.out.println(p1.name + "'s health is at " + p1.health + ".");
         }
+
+        turn = false;
     }
-    //player turn method
+
     public void turn(){
         //choose what to do; attack, defend, item?
         choice = user.getString();
         if(choice.equals("attack")){
-            //choose who to attack
-            System.out.print("Which pokemon would you like to attack? ");
-            pkmnName = user.getString();
-            System.out.println(pkmnName);
-            attack(); //place holder name convert to user input
-        }
-        else if (choice.equals("defend")){
-            this.defend();
+            this.attack();
         }
         else if (choice.equals("item")){
             System.out.println("You used a potion.");
-            if (health > 80){
-                //heals up to 100
-                margin = 100 - health;
-                health = health + margin;
-                System.out.println(name + "'s health is now " + health);
-                turn = false; //ends turn
-            }
-            else if (health <= 80){
-                health = health += 20; //heals 20 health
-                System.out.println(name + "'s health is now " + health);
-                turn = false; //ends turn
-            }
-            else{
+            turn = false;
+            if (health == 100){
                 System.out.println("The potion had no effect. You are already at max health.");
                 turn = false; //ends turn
             }
+            else if (health > 80){
+                //heals up to 100
+                margin = 100 - health;
+                health = health + margin; 
+                System.out.println(health);
+                turn = false; //ends turn
+            }
+            else{
+                health += 20; //heals 20 health
+                System.out.println(health);
+                turn = false; //ends turn
+            }
         }
-        //debug is used for checking components that rely on user choices
-        else if (choice.equals("debug")){
-            System.out.println("---Debug Menu---\nthis.name: " + name +"\nShadow: " + shadow + "\nshadowCount: " + shadowCount + 
-                "\nPlayer Health (1,2,3): " + p1.health + " " + p2.health + " " + p3.health + "\nLast Item Use Margin: " +
-                margin + "\n pkmnName: " + pkmnName + "\n---End Debug Menu---");
-        }
-
     }
 }
