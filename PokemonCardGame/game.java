27aaf93688cf26322d71f6b6b5a74267cd4a0e0f
Zach Zimmerman <zzimmerman-student@nfcs.k12.pa.us>
2018-01-28 23:15:08 -0500

    Add files via upload

--- PokemonCardGame/game.java
+++ PokemonCardGame/game.java
@@ -11,46 +11,60 @@ public class game
     //Determines number of players
     static int numOfPlayers = 2;
     //Simple "coin toss" to decide who goes first
-    static int playerTurn = (int)(Math.random() * numOfPlayers);
-    static int currentPlayer;
+    static int currentPlayer = (int)(Math.random() * numOfPlayers);
+    static int p1Health;
+    static int p2Health;
+    static String temp;
+    static String p1Name;
+    static String p2Name;
     public static void play(){
         d.shuffle(d.pokemon);
         player p1 = new player();
         player p2 = new player();
+        p1Health = p1.health;
+        p2Health = p2.health;
         //give players pokemon
         System.out.println("---Pokemon Names---");//seperation code
+        System.out.print("Player 1: ");
         p1.setName(d.pokemon[0]);
+        System.out.print("Player 2: ");
         p2.setName(d.pokemon[1]);
+        System.out.print("\n");
+        temp = p1.name;
+        p1.name = p2.name;
+        p2.name = temp;
+        p1Name = p2.name;
+        p2Name = p1.name;
+        boolean gameInProgress = true;
         //Game is active until one player's health = 0
-        while (p1.health >= 0 || p2.health >= 0){
-            //tells program who's turn it is
-            if (playerTurn == 0){ 
-                p1.turn = true;
-                currentPlayer = 0;
-            }
-            else if (playerTurn == 1){
-                p2.turn = true;
-                currentPlayer = 1;
-            }
-
-            while (p1.turn){
-                System.out.print("Player 1, would you like to attack or use item?");
+        while (gameInProgress){
+            while (currentPlayer == 0){
+                System.out.print("Player 1, would you like to attack or use item? ");
+                temp = p1.name;
                 p1.turn();
-                p1.turn = false;
-                p2.turn = true;
+                p2.health = p2Health;
                 currentPlayer = 1;
                 break;
             }
 
-            while (p2.turn){
-                System.out.print("Player 2, would you like to attack or use item?");
+            while (currentPlayer == 1){
+                System.out.print("Player 2, would you like to attack or use item? ");
+                temp = p2.name;
                 p2.turn();
-                p2.turn = false;
-                p1.turn = true;
+                p1.health = p1Health;
                 currentPlayer = 0;
                 break;
             }
 
+            if(p1Health <= 0){
+                gameInProgress = false;
+                System.out.println("Player 1's pokemon has 0 health remaining. Player 2 wins!");
+            }
+            if(p2Health <= 0){
+                gameInProgress = false;
+                System.out.println("Player 2's pokemon has 0 health remaining. Player 1 wins!");
+            }
         }
     }
+
 }
--- PokemonCardGame/player.java
+++ PokemonCardGame/player.java
@@ -7,63 +7,95 @@
  */
 public class player extends game
 {
-    player p1;
-    player p2;
     //base 100 health
     int health = 100;
     deck d = new deck();
     scanner user = new scanner();
-    private String name;
+    String name;
     //hardcoded as a temp user choice
     private String choice;
+    int damage;
     //says when it's a player's turn
-    boolean turn = false;
+    //boolean turn = false;
     private int margin = 0;
     //gives player's pokemon names
-    public void setName(String t){
-        name = t;
-        System.out.println(name + " " + health + " health "); //test code?
+    public void setName(String n){
+        name = n;
+        System.out.println(name + " " + health + " health ");
     }
 
     public void attack(){
         //random damage
-        int damage = (int) (Math.random() * 15 + 15); //+ 15 is needed to keep damage from being 0. range = 15 to 30.
+        damage = (int) (Math.random() * 15 + 15); //+ 15 is needed to keep damage from being 0.
         if (currentPlayer == 0){
-            p2.health = p2.health - damage;
-            System.out.println(p2.name + "'s health is at " + p2.health + ".");
+            p2Health = p2Health - damage;
+            if(p2Health < 0){
+                p2Health = 0;
+                health = p2Health;
+            }
+            System.out.println(name + "'s health is now " + p2Health + ".");
         }
         else{
-            p1.health = p1.health - damage;
-            System.out.println(p1.name + "'s health is at " + p1.health + ".");
+            p1Health = p1Health - damage;
+            if(p1Health < 0){
+                p1Health = 0;
+                health = p1Health;
+            }
+            System.out.println(name + "'s health is now " + p1Health + ".");
         }
-
-        turn = false;
     }
 
     public void turn(){
         //choose what to do; attack, defend, item?
         choice = user.getString();
         if(choice.equals("attack")){
-            this.attack();
+            attack();
+        }
+        else if(choice.equals("debug")){
+            if(currentPlayer == 0){
+                System.out.print("---Debug Menu---\nName: " + p1Name + "\nHealth: " + health + "\np1Health: " + p1Health + "\np2Health: " + p2Health + "\nDamage last Turn: " + damage + "\n");
+            }
+            else{
+                System.out.print("---Debug Menu---\nName: " + p2Name + "\nHealth: " + health + "\np1Health: " + p1Health + "\np2Health: " + p2Health + "\nDamage last Turn: " + damage + "\n");
+            }
         }
         else if (choice.equals("item")){
             System.out.println("You used a potion.");
-            turn = false;
-            if (health == 100){
-                System.out.println("The potion had no effect. You are already at max health.");
-                turn = false; //ends turn
-            }
-            else if (health > 80){
-                //heals up to 100
-                margin = 100 - health;
-                health = health + margin; 
-                System.out.println(health);
-                turn = false; //ends turn
+            if(currentPlayer == 0){
+                health = p1Health;
+                if (health == 100){
+                    System.out.println("The potion had no effect. You are already at max health.");
+                }
+                else if (health > 80){
+                    //heals up to 100
+                    margin = 100 - health;
+                    health = health += margin; 
+                    p1Health = health;
+                    System.out.println(p1Name + "'s health is now " + health + ".");
+                }
+                else{
+                    health += 20; //heals 20 health
+                    p1Health = health;
+                    System.out.println(p1Name + "'s health is now " + health + ".");
+                }
             }
             else{
-                health += 20; //heals 20 health
-                System.out.println(health);
-                turn = false; //ends turn
+                health = p2Health;
+                if (health == 100){
+                    System.out.println("The potion had no effect. You are already at max health.");
+                }
+                else if (health > 80){
+                    //heals up to 100
+                    margin = 100 - health;
+                    health = health += margin; 
+                    p2Health = health;
+                    System.out.println(p2Name + "'s health is now  at " + health + ".");
+                }
+                else{
+                    health += 20; //heals 20 health
+                    p2Health = health;
+                    System.out.println(p2Name + "'s health is now  at " + health + ".");
+                }   
             }
         }
     }
